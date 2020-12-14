9cacd10da182791bb1a9c371290329dc5ad82674
jasontsaicc <jasontsaicc@gmail.com>
2020-12-14 17:24:29 +0800

    淺拷貝不會對子對象進行拷貝

--- hm_09_淺拷貝.py
+++ hm_09_淺拷貝.py
@@ -20,3 +20,21 @@ print("my_tuple1:", id(my_tuple1), "my_tuple2", id(my_tuple2))
 
 # 淺拷貝不會對不可變類型進行拷貝，也就說不會開闢內存內存空間，
 # 對於不可變類型進行淺拷貝實際，是對引用的一個拷貝，兩個變量指定的是一個內存地址
+
+# 可變類型： 列表，字典，集合
+my_list1 = [1, 3, [4, 6]]
+
+my_list2 = copy.copy(my_list1)
+
+print("my_list1:", id(my_list1), "my_list2", id(my_list2))
+print(my_list1)
+print(my_list2)
+
+my_list1.append(5)
+print(my_list1, my_list2)
+
+# 淺拷貝不會對子對象進行拷貝
+print("my_list1[2]:", id(my_list1[2]), "my_list2[2]", id(my_list2[2]))
+my_list1[2].append(3)
+print(my_list1)
+print(my_list2)
