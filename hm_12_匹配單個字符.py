364271ada4b811cd3d824c887de476f41230dd81
jasontsaicc <jasontsaicc@gmail.com>
2020-12-15 15:38:22 +0800

    1.使用 "."匹配任意1个字符—加入if else語句

--- hm_12_匹配單個字符.py
+++ hm_12_匹配單個字符.py
@@ -10,6 +10,10 @@ import re
 # \W。	匹配特殊字符，即非字母、非数字、非汉字
 
 # 1.匹配任意1个字符（除了\n）
-match_obj = re.match("t.o", "two")
-result = match_obj.group()
-print(result)
+match_obj = re.match("t.o", "t/no")
+if match_obj:
+    result = match_obj.group()
+    print(result)
+else:
+    # 匹配失敗match_obj是一個None
+    print("匹配失敗")
